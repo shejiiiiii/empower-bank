dcf465d84cb0303132fab829a94e420ebfbeb4d7
shejiiiiii <carljamesbenitez12@gmail.com>
2026-08-20 15:12:18 +0800

    Fix empowerbank bat

--- empowerbank.bat
+++ empowerbank.bat
@@ -1,5 +1,3 @@
 @echo off
 
-start "" c:\cygwin64\bin\mintty.exe
-/bin/bash -l -c "cd /home/carl.james.a.benitez/empower-bank/empower-bank && ./batches/statement_report_batch.sh"
-
+"./batches/statement_report_batch.sh"
